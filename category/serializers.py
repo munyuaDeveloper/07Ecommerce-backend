8228b7dae293d3bedf28231959085054d8b85ac8
Gathiira <mwangyjose@gmail.com>
2021-05-25 11:12:07 +0300

    create and list category

--- category/serializers.py
+++ category/serializers.py
@@ -2,10 +2,22 @@ from category.models import Category
 from rest_framework import serializers
 
 
-class CategorySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Category
-        fields = ['name']
+class CategorySerializer(serializers.Serializer):
+    name = serializers.CharField()
 
-    def to_representation(self, instance):
-        return instance.name
+    def create(self, validated_data):
+        name = validated_data.get('name')
+        name = name.upper()
+        category = Category.objects.filter(name__icontains=name)
+        if category.exists():
+            category = category.first()
+            response = {
+                "name": category.name
+            }
+            return response
+
+        Category.objects.create(name=name)
+        response = {
+            "name": name
+        }
+        return response
--- config/urls.py
+++ config/urls.py
@@ -25,8 +25,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('users.urls')),
     path('products/', include('product.urls')),
-     path('category/', include('category.urls')),
+    path('category/', include('category.urls')),
     path('documentation/', schema_view),
 ]
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
