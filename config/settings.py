fbe8c7ad3f2d0d142de134ce921d31bf68e7ed28
munyuaDeveloper <munyuapeter06@gmail.com>
2021-09-21 06:56:50 +0300

    change the shared service link

--- config/settings.py
+++ config/settings.py
@@ -190,8 +190,8 @@ MEDIA_URL = '/media/'
 
 
 SERVICE_URLS = {
-    "shared_service": "https://ecommerce-apis.herokuapp.com/"
-    # "shared_service": "http://localhost:1400/"
+    # "shared_service": "https://ecommerce-apis.herokuapp.com/"
+    "shared_service": "http://52.66.197.147/"
 }
 
 django_heroku.settings(locals())
